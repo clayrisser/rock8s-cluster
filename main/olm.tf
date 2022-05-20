98ed0deab3af5c46729c7eab1a67644aa7adc6fd
Clay Risser <clayrisser@gmail.com>
2022-05-20 14:49:53 -0500

    Updated olm

--- main/olm.tf
+++ main/olm.tf
@@ -4,39 +4,55 @@
  * File Created: 17-04-2022 06:13:18
  * Author: Clay Risser
  * -----
- * Last Modified: 30-04-2022 12:28:11
+ * Last Modified: 20-05-2022 14:41:34
  * Modified By: Clay Risser
  * -----
  * Risser Labs LLC (c) Copyright 2022
  */
 
-resource "kubernetes_namespace" "sn_system" {
-  metadata {
-    name = "sn-system"
-  }
-  depends_on = [
-    null_resource.wait_for_nodes
-  ]
-  lifecycle {
-    prevent_destroy = false
-    ignore_changes  = []
-  }
+locals {
+  olm_version = "v0.21.2"
 }
 
 module "olm" {
-  source                        = "streamnative/charts/helm"
-  version                       = "0.8.1"
-  enable_function_mesh_operator = false
-  enable_istio_operator         = false
-  enable_kiali_operator         = false
-  enable_olm                    = true
-  enable_otel_collector         = false
-  enable_prometheus_operator    = false
-  enable_pulsar_operator        = false
-  enable_vault_operator         = false
-  enable_vector_agent           = false
-  enable_vmagent                = false
+  source     = "../modules/kubectl_apply"
+  kubeconfig = local.kubeconfig
+  resources = [
+    "https://raw.githubusercontent.com/operator-framework/operator-lifecycle-manager/${local.olm_version}/deploy/upstream/quickstart/crds.yaml",
+    "https://raw.githubusercontent.com/operator-framework/operator-lifecycle-manager/${local.olm_version}/deploy/upstream/quickstart/olm.yaml"
+  ]
   depends_on = [
-    kubernetes_namespace.sn_system
+    null_resource.wait_for_nodes
   ]
 }
+
+# resource "kubernetes_namespace" "sn_system" {
+#   metadata {
+#     name = "sn-system"
+#   }
+#   depends_on = [
+#     null_resource.wait_for_nodes
+#   ]
+#   lifecycle {
+#     prevent_destroy = false
+#     ignore_changes  = []
+#   }
+# }
+
+# module "olm" {
+#   source                        = "streamnative/charts/helm"
+#   version                       = "0.8.1"
+#   enable_function_mesh_operator = false
+#   enable_istio_operator         = false
+#   enable_kiali_operator         = false
+#   enable_olm                    = true
+#   enable_otel_collector         = false
+#   enable_prometheus_operator    = false
+#   enable_pulsar_operator        = false
+#   enable_vault_operator         = false
+#   enable_vector_agent           = false
+#   enable_vmagent                = false
+#   depends_on = [
+#     kubernetes_namespace.sn_system
+#   ]
+# }
