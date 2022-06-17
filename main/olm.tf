48e1bfab61d9ebcf3f53b7aa2b13ceeb120b58c7
Clay Risser <clayrisser@gmail.com>
2022-06-17 10:46:50 -0500

    Fixed olm

--- main/olm.tf
+++ main/olm.tf
@@ -4,7 +4,7 @@
  * File Created: 17-04-2022 06:13:18
  * Author: Clay Risser
  * -----
- * Last Modified: 11-06-2022 05:20:37
+ * Last Modified: 11-06-2022 09:43:51
  * Modified By: Clay Risser
  * -----
  * Risser Labs LLC (c) Copyright 2022
@@ -14,55 +14,19 @@ locals {
   olm_version = "v0.21.2"
 }
 
-module "olm_crds" {
-  source     = "../modules/kubectl_apply"
-  kubeconfig = local.kubeconfig
-  resources = [
-    "https://raw.githubusercontent.com/operator-framework/operator-lifecycle-manager/${local.olm_version}/deploy/upstream/quickstart/crds.yaml"
-  ]
-  depends_on = [
-    null_resource.wait_for_nodes
-  ]
+resource "rancher2_app_v2" "olm" {
+  chart_name    = "olm"
+  chart_version = "0.21.2"
+  cluster_id    = local.rancher_cluster_id
+  name          = "olm"
+  namespace     = "olm"
+  repo_name     = rancher2_catalog_v2.risserlabs.name
+  wait          = true
+  values        = <<EOF
+EOF
+  depends_on    = []
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes  = []
+  }
 }
-
-module "olm" {
-  source     = "../modules/kubectl_apply"
-  kubeconfig = local.kubeconfig
-  resources = [
-    "https://raw.githubusercontent.com/operator-framework/operator-lifecycle-manager/${local.olm_version}/deploy/upstream/quickstart/olm.yaml"
-  ]
-  depends_on = [
-    module.olm_crds
-  ]
-}
-
-# resource "kubernetes_namespace" "sn_system" {
-#   metadata {
-#     name = "sn-system"
-#   }
-#   depends_on = [
-#     null_resource.wait_for_nodes
-#   ]
-#   lifecycle {
-#     prevent_destroy = false
-#     ignore_changes  = []
-#   }
-# }
-
-# module "olm" {
-#   source                        = "streamnative/charts/helm"
-#   version                       = "0.8.1"
-#   enable_function_mesh_operator = false
-#   enable_istio_operator         = false
-#   enable_kiali_operator         = false
-#   enable_olm                    = true
-#   enable_otel_collector         = false
-#   enable_prometheus_operator    = false
-#   enable_pulsar_operator        = false
-#   enable_vault_operator         = false
-#   enable_vector_agent           = false
-#   enable_vmagent                = false
-#   depends_on = [
-#     kubernetes_namespace.sn_system
-#   ]
-# }
