a2f3b1be57a99f001b21f1bef1e6e622993cb920
Clay Risser <clayrisser@gmail.com>
2022-07-06 15:01:10 -0500

    Updated cert-manager ns

--- main/cluster_issuer.tf
+++ main/cluster_issuer.tf
@@ -4,7 +4,7 @@
  * File Created: 07-05-2022 03:17:43
  * Author: Clay Risser
  * -----
- * Last Modified: 06-07-2022 06:38:39
+ * Last Modified: 06-07-2022 14:29:31
  * Modified By: Clay Risser
  * -----
  * Risser Labs LLC (c) Copyright 2022
@@ -15,7 +15,7 @@ resource "rancher2_app_v2" "cluster_issuer" {
   chart_version = "1.1.0"
   cluster_id    = local.rancher_cluster_id
   name          = "cluster-issuer"
-  namespace     = "cert-manager"
+  namespace     = "kube-system"
   repo_name     = rancher2_catalog_v2.risserlabs.name
   wait          = true
   values        = <<EOF
