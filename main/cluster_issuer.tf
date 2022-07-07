7661230a6b1b4a4f6800ef5aef8113c407835fec
Clay Risser <clayrisser@gmail.com>
2022-07-07 09:43:36 -0500

    Fixed depends on

--- main/cluster_issuer.tf
+++ main/cluster_issuer.tf
@@ -4,7 +4,7 @@
  * File Created: 07-05-2022 03:17:43
  * Author: Clay Risser
  * -----
- * Last Modified: 06-07-2022 14:29:31
+ * Last Modified: 07-07-2022 09:43:31
  * Modified By: Clay Risser
  * -----
  * Risser Labs LLC (c) Copyright 2022
@@ -24,6 +24,9 @@ config:
   clusterType: rke
   email: ${var.cloudflare_email}
 EOF
+  depends_on = [
+    rancher2_app_v2.integration_operator
+  ]
   lifecycle {
     prevent_destroy = false
     ignore_changes  = []
