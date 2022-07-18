749fd2ad0bb56299231151d047c25e495ce22078
Clay Risser <clayrisser@gmail.com>
2022-07-18 14:17:53 -0500

    Updated cluster tls

--- main/tls.tf
+++ main/tls.tf
@@ -4,7 +4,7 @@
  * File Created: 27-04-2022 12:01:36
  * Author: Clay Risser
  * -----
- * Last Modified: 18-07-2022 03:45:06
+ * Last Modified: 18-07-2022 14:17:21
  * Modified By: Clay Risser
  * -----
  * Risser Labs LLC (c) Copyright 2022
@@ -19,7 +19,7 @@ resource "tls_self_signed_cert" "ca" {
   private_key_pem   = tls_private_key.ca.private_key_pem
   is_ca_certificate = true
   subject {
-    common_name = "kubernetes-ca"
+    common_name = "kubernetes-ca-ca"
   }
   validity_period_hours = 43800
   allowed_uses = [
@@ -39,7 +39,7 @@ resource "tls_self_signed_cert" "root_ca" {
   private_key_pem   = tls_private_key.root_ca.private_key_pem
   is_ca_certificate = true
   subject {
-    common_name = "kubernetes-ca"
+    common_name = "kubernetes-ca-root"
   }
   validity_period_hours = 43800
   allowed_uses = [
@@ -59,7 +59,7 @@ resource "tls_self_signed_cert" "client_ca" {
   private_key_pem   = tls_private_key.client_ca.private_key_pem
   is_ca_certificate = true
   subject {
-    common_name = "kubernetes-ca"
+    common_name = "kubernetes-ca-client"
   }
   validity_period_hours = 43800
   allowed_uses = [
