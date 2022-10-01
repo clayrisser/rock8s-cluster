cf23cbe293c4049207b7dfe73fa76d6a30ff7118
Clay Risser <clayrisser@gmail.com>
2022-10-01 04:04:32 -0500

    Added default issuer

--- main/cluster_issuer.tf
+++ main/cluster_issuer.tf
@@ -4,7 +4,7 @@
  * File Created: 07-05-2022 03:17:43
  * Author: Clay Risser
  * -----
- * Last Modified: 29-09-2022 12:41:23
+ * Last Modified: 01-10-2022 04:04:14
  * Modified By: Clay Risser
  * -----
  * Risser Labs LLC (c) Copyright 2022
@@ -28,4 +28,42 @@ EOF
   depends_on = [
     module.integration_operator
   ]
+}
+
+resource "kubectl_manifest" "cert_manager_default_issuer" {
+  count     = (var.cluster_issuer && var.patch_operator) ? 1 : 0
+  yaml_body = <<EOF
+apiVersion: patch.risserlabs.com/v1alpha1
+kind: Patch
+metadata:
+  name: cert-manager-default-issuer
+  namespace: kube-system
+spec:
+  patches:
+    - id: cloud-credentials
+      type: json
+      waitForResource: true
+      target:
+        apiVersion: apps/v1
+        kind: Deployment
+        name: cert-manager
+      patch: |
+        - op: replace
+          path: /spec/template/spec/containers/0/args
+          value: |
+            - --v=2
+            - --cluster-resource-namespace=$(POD_NAMESPACE)
+            - --leader-election-namespace=kube-system
+            - --enable-certificate-owner-ref=true
+            - --default-issuer-name=letsencrypt-cloudflare-prod
+            - --default-issuer-kind=ClusterIssuer
+            - --default-issuer-group=cert-manager.io
+EOF
+  depends_on = [
+    module.cluster_issuer
+  ]
+  lifecycle {
+    prevent_destroy = false
+    ignore_changes  = []
+  }
 }
