8831adf8ef68fdb5486f1ff2d575f01673761c3f
Clay Risser <clayrisser@gmail.com>
2022-10-07 09:17:03 -0500

    Improved lifecycle rules

--- main/buckets.tf
+++ main/buckets.tf
@@ -4,7 +4,7 @@
  * File Created: 18-09-2022 08:43:29
  * Author: Clay Risser
  * -----
- * Last Modified: 03-10-2022 13:47:52
+ * Last Modified: 07-10-2022 09:16:48
  * Modified By: Clay Risser
  * -----
  * Risser Labs LLC (c) Copyright 2022
@@ -13,41 +13,76 @@
 resource "aws_s3_bucket" "main" {
   bucket        = var.main_bucket == "" ? local.cluster_name : var.main_bucket
   force_destroy = true
+  lifecycle {
+    prevent_destroy = false
+  }
 }
 
 resource "aws_s3_bucket" "tempo" {
   bucket        = var.tempo_bucket == "" ? "tempo.${local.cluster_name}" : var.tempo_bucket
   force_destroy = true
-  lifecycle_rule {
-    id      = "retention"
-    prefix  = "single-tenant/"
-    enabled = true
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "tempo" {
+  bucket = aws_s3_bucket.tempo.id
+  rule {
+    id     = "retention"
+    status = "Enabled"
+    filter {
+      prefix = "single-tenant/"
+    }
     expiration {
       days = ceil(var.retention_hours / 24)
     }
+  }
+  lifecycle {
+    prevent_destroy = false
   }
 }
 
 resource "aws_s3_bucket" "thanos" {
   bucket        = var.thanos_bucket == "" ? "thanos.${local.cluster_name}" : var.thanos_bucket
   force_destroy = true
-  lifecycle_rule {
-    id      = "retention"
-    enabled = true
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "thanos" {
+  bucket = aws_s3_bucket.thanos.id
+  rule {
+    id     = "retention"
+    status = "Enabled"
     expiration {
       days = ceil(var.retention_hours / 24)
     }
+  }
+  lifecycle {
+    prevent_destroy = false
   }
 }
 
 resource "aws_s3_bucket" "loki" {
   bucket        = var.loki_bucket == "" ? "loki.${local.cluster_name}" : var.loki_bucket
   force_destroy = true
-  lifecycle_rule {
-    id      = "retention"
-    enabled = true
+  lifecycle {
+    prevent_destroy = false
+  }
+}
+
+resource "aws_s3_bucket_lifecycle_configuration" "loki" {
+  bucket = aws_s3_bucket.loki.id
+  rule {
+    id     = "retention"
+    status = "Enabled"
     expiration {
       days = ceil(var.retention_hours / 24)
     }
+  }
+  lifecycle {
+    prevent_destroy = false
   }
 }
