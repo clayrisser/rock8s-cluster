606d9f057489c8ab3d7addce6b942a6b12286fee
Clay Risser <clayrisser@gmail.com>
2022-10-15 01:08:44 -0500

    Test small deployment

--- main/cluster.tf
+++ main/cluster.tf
@@ -4,7 +4,7 @@
  * File Created: 14-04-2022 08:13:23
  * Author: Clay Risser
  * -----
- * Last Modified: 14-10-2022 14:21:38
+ * Last Modified: 15-10-2022 01:08:26
  * Modified By: Clay Risser
  * -----
  * Risser Labs LLC (c) Copyright 2022
@@ -232,8 +232,8 @@ resource "kops_instance_group" "t3-medium-a" {
   name                       = "t3-medium-a"
   autoscale                  = true
   role                       = "Node"
-  min_size                   = 2
-  max_size                   = 4
+  min_size                   = 1
+  max_size                   = 3
   machine_type               = "t3.medium"
   subnets                    = [data.aws_subnet.public[0].id]
   additional_security_groups = [aws_security_group.nodes.id]
@@ -249,7 +249,7 @@ resource "kops_instance_group" "t3-medium-b" {
   autoscale                  = true
   role                       = "Node"
   min_size                   = 1
-  max_size                   = 4
+  max_size                   = 3
   machine_type               = "t3.medium"
   subnets                    = [data.aws_subnet.public[1].id]
   additional_security_groups = [aws_security_group.nodes.id]
@@ -265,7 +265,7 @@ resource "kops_instance_group" "t3-medium-c" {
   autoscale                  = true
   role                       = "Node"
   min_size                   = 1
-  max_size                   = 4
+  max_size                   = 3
   machine_type               = "t3.medium"
   subnets                    = [data.aws_subnet.public[2].id]
   additional_security_groups = [aws_security_group.nodes.id]
