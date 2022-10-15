ed423550676b91a751a1ce425e9ac8656db31a14
Clay Risser <clayrisser@gmail.com>
2022-10-15 02:27:04 -0500

    Disabled goldilocks

--- main/goldilocks.tf
+++ main/goldilocks.tf
@@ -4,32 +4,32 @@
  * File Created: 20-04-2022 10:21:22
  * Author: Clay Risser
  * -----
- * Last Modified: 14-10-2022 04:58:25
+ * Last Modified: 15-10-2022 02:26:57
  * Modified By: Clay Risser
  * -----
  * Risser Labs LLC (c) Copyright 2022
  */
 
-module "goldilocks" {
-  source             = "../modules/helm_release"
-  enabled            = var.goldilocks
-  chart_version      = "6.1.1"
-  name               = "goldilocks"
-  repo               = "https://charts.fairwinds.com/stable"
-  chart_name         = "goldilocks"
-  namespace          = "goldilocks"
-  create_namespace   = true
-  rancher_project_id = local.rancher_project_id
-  rancher_cluster_id = local.rancher_cluster_id
-  values             = <<EOF
-vpa:
-  enabled: true
-  updater:
-    enabled: false
-dashboard:
-  excludeContainers: 'linkerd-proxy,istio-proxy'
-EOF
-  depends_on = [
-    null_resource.wait_for_nodes
-  ]
-}
+# module "goldilocks" {
+#   source             = "../modules/helm_release"
+#   enabled            = var.goldilocks
+#   chart_version      = "6.1.1"
+#   name               = "goldilocks"
+#   repo               = "https://charts.fairwinds.com/stable"
+#   chart_name         = "goldilocks"
+#   namespace          = "goldilocks"
+#   create_namespace   = true
+#   rancher_project_id = local.rancher_project_id
+#   rancher_cluster_id = local.rancher_cluster_id
+#   values             = <<EOF
+# vpa:
+#   enabled: true
+#   updater:
+#     enabled: false
+# dashboard:
+#   excludeContainers: 'linkerd-proxy,istio-proxy'
+# EOF
+#   depends_on = [
+#     null_resource.wait_for_nodes
+#   ]
+# }
