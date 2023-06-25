d1d83942cf55507ac0926e51d276ac2d48853cbb
Clay Risser <clayrisser@gmail.com>
2023-06-25 07:17:37 -0400

    Updated easy olm operator

---
+++ main/easy_olm_operator.tf
@@ -0,0 +1,25 @@
+/**
+ * File: /main/easy_olm_operator.tf
+ * Project: kops
+ * File Created: 21-04-2022 09:05:39
+ * Author: Clay Risser
+ * -----
+ * Last Modified: 25-06-2023 07:17:13
+ * Modified By: Clay Risser
+ * -----
+ * Risser Labs LLC (c) Copyright 2022
+ */
+
+module "easy_olm_operator" {
+  source             = "../modules/helm_release"
+  enabled            = local.integration_operator
+  chart_name         = "easy-olm-operator"
+  chart_version      = "0.0.1"
+  name               = "easy-olm-operator"
+  namespace          = "kube-system"
+  repo               = module.risserlabs_repo.repo
+  rancher_cluster_id = local.rancher_cluster_id
+  depends_on = [
+    kubernetes_secret.registry
+  ]
+}
