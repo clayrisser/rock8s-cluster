73c5d909cd05501a5c1ce25491742c7af1a7fd6e
Clay Risser <clayrisser@gmail.com>
2023-06-26 07:23:46 -0400

    ✨ feat: Upgrade chart version to 0.2.0 in integration_operator module
    The chart version in the integration_operator module has been updated from 0.1.2 to 0.2.0. The change was made by Clay Risser on 26-06-2023 at 07:22:49.

--- main/integration_operator.tf
+++ main/integration_operator.tf
@@ -4,7 +4,7 @@
  * File Created: 21-04-2022 09:05:39
  * Author: Clay Risser
  * -----
- * Last Modified: 26-06-2023 07:20:39
+ * Last Modified: 26-06-2023 07:22:49
  * Modified By: Clay Risser
  * -----
  * Risser Labs LLC (c) Copyright 2022
@@ -14,7 +14,7 @@ module "integration_operator" {
   source             = "../modules/helm_release"
   enabled            = local.integration_operator
   chart_name         = "integration-operator"
-  chart_version      = "0.1.2"
+  chart_version      = "0.2.0"
   name               = "integration-operator"
   namespace          = "kube-system"
   repo               = module.rock8s_repo.repo
